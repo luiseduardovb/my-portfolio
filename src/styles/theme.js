3f3b228b995cd86fb2634c3bc16bb8b5489276b1
luiseduardovb <le_velasquez@yahoo.com>
2021-08-12 20:01:13 -0500

    Add Home Section

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,12 +2,14 @@ import { createTheme } from "@material-ui/core/styles";
 
 const levLightBlue = "#46A2D5";
 const levWhite = "#ffffff";
+const levBlack = "#1E1E20";
 
 const theme = createTheme({
   palette: {
     common: {
       lightBlue: levLightBlue,
       white: levWhite,
+      black: levBlack,
     },
     primary: {
       main: levLightBlue,
